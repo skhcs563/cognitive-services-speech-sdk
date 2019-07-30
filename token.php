f90cba0214a87e9eb4f9f3d4b82d4c48b3909748
skhcs563 <51762551+skhcs563@users.noreply.github.com>
2019-07-30 23:02:34 +0900

    2

---
+++ token.php
@@ -0,0 +1,15 @@
+<?php
+header('Access-Control-Allow-Origin: ' . $_SERVER['SERVER_NAME']);
+
+// Replace with your own subscription key and service region (e.g., "westus").
+$subscriptionKey = 'YourSubscriptionKey';
+$region = 'YourServiceRegion';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken');
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, '{}');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Ocp-Apim-Subscription-Key: ' . e0bcab1729ed458c9d274ed353a64ee5)); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+echo curl_exec($ch);
+?>
